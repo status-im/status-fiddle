071e2b66b507d4d2fbd6f66c1b611fc558329023
Foo Pang <foo.pang@gmail.com>
2018-01-02 21:45:07 +0800

    Configure project so react-native-web npm lib can be used in reagent forms

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+window.deps = {
+  'react' : require('react'),
+  'react-dom' : require('react-dom'),
+  'react-native-web' : require('react-native-web'),
+};
+
+window.React = window.deps['react'];
+window.ReactDOM = window.deps['react-dom'];
+window.ReactNativeWeb = window.deps['react-native-web'];
